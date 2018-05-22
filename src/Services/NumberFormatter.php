4f55839542e0bf26b9a5dbe779f6c87d4d22df3f
Vitalijus Zudenkovas <zudenkovas@gmail.com>
2018-05-22 22:38:29 +0300

    code diet :)

--- src/Services/NumberFormatter.php
+++ src/Services/NumberFormatter.php
@@ -14,48 +14,37 @@ class NumberFormatter
 
     public function format($number)
     {
-        if ($number >= 999500 or $number <= -999500) {
-            $rounded = round($number, -4);
-            $tostring = number_format($rounded, 2, '.', '.');
-            $shorten = mb_substr($tostring, 0, -8) . 'M';
-
-            return $shorten;
+        $sign = $number < 0 ? '-' : '';
+        $absNumber = abs($number);
+        if ($absNumber >= 999500 ) {
+            return $sign . sprintf('%.2fM', round($absNumber / 1000000, 2));
 
         }
 
-        if (($number < 999500 and $number >= 99950) or ($number > -999500 and $number <= -99950)) {
-            $rounded = round($number, -3);
-            $tostring = number_format($rounded, 2, '', '');
-            $shorten = mb_substr($tostring, 0, -5) . 'K';
-
-            return $shorten;
+        if ($absNumber < 999500 and $absNumber >= 99950) {
+            return $sign . sprintf('%.0fK', round($absNumber / 1000, 0));
 
         }
 
-        if (($number < 99950 and $number >= 1000) or ($number > -99950 and $number <= -1000)) {
-            $rounded = round($number, 2);
-            $tostring = number_format($rounded, 0, '', ' ');
-            $shorten = mb_substr($tostring, 0);
-
-            return $shorten;
+        if ($absNumber < 99950 and $absNumber >= 1000) {
+            return $sign . number_format($absNumber, 0, '', ' ');
 
         }
 
-        if (($number < 1000 and $number >= 0) or ($number > -1000 and $number < 0)) {
+        if ($absNumber < 1000 and $absNumber >= 0) {
 
 
-            $rounded = round($number, 2);
+            $rounded = round($absNumber, 2);
 
 
-            if ($rounded === 1000.00 or $rounded === -1000.00) {
-                $tostring = number_format($rounded, 0, '.', ' ');
-                $shorten = mb_substr($tostring, 0);
+            if ($rounded === 1000.00) {
+                return $sign . number_format($rounded, 0, '.', ' ');
+
             } else {
-                $tostring = number_format($rounded, 2, '.', ' ');
-                $shorten = mb_substr($tostring, 0);
+                return $sign . number_format($rounded, 2, '.', ' ');
+
             }
 
-            return $shorten;
 
         }
 
